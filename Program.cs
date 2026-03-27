b99b9665c73342e14ef54c51fe7110734d5e91c1
vinod kumar <vinod.kumar@beo-software.in>
2026-03-27 15:14:07 +0530

    changed weatherforecast size to check cicd

--- Program.cs
+++ Program.cs
@@ -31,7 +31,7 @@ var summaries = new[]
 
 app.MapGet("/weatherforecast", () =>
 {
-    var forecast = Enumerable.Range(1, 5).Select(index =>
+    var forecast = Enumerable.Range(1, 10).Select(index =>
         new WeatherForecast
         (
             DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
